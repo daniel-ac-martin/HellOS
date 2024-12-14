e2be196b8ffaabb908c02b51f4104ccc17629599
Daniel A.C. Martin <github@daniel-martin.co.uk>
2024-12-14 21:39:33 +0000

    Add build script

--- README.md
+++ README.md
@@ -5,11 +5,11 @@ Bare bones "hello world" i386 kernel written in [Zig](https://ziglang.org/).
 ## Building
 
 ```
-zig build-exe hellos.zig -target x86-freestanding -T linker.ld -O ReleaseSmall
+zig build --release=small
 ```
 
 ## Testing with qemu
 
 ```
-qemu-system-i386 -kernel hellos
+qemu-system-i386 -kernel zig-out/bin/HellOs
 ```
---
+++ build.zig
@@ -0,0 +1,25 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{ .default_target = .{ .os_tag = .freestanding, .abi = .eabi, .ofmt = .elf, .cpu_arch = .x86 } });
+    const optimize = b.standardOptimizeOption(.{});
+
+    const kernel = b.addExecutable(.{
+        .name = "HellOS",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    kernel.setLinkerScriptPath(b.path("linker.ld"));
+
+    b.installArtifact(kernel);
+
+    const unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_unit_tests.step);
+}
